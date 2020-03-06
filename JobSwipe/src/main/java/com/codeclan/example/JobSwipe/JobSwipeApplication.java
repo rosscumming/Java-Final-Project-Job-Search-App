b62b8d1dd6c1d11c10feca9657d646db860394a1
Gary Clark <gclark04432@live.co.uk>
2020-03-06 13:29:16 +0000

    create react front end and spring back end project outlines

---
+++ JobSwipe/src/main/java/com/codeclan/example/JobSwipe/JobSwipeApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.JobSwipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSwipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSwipeApplication.class, args);
+	}
+
+}
---
+++ JobSwipe/src/test/java/com/codeclan/example/JobSwipe/JobSwipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.JobSwipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSwipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
