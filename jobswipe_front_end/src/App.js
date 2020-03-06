9192e4a05591c110ccacaf87d6bb443aee147ab1
Ross <rosso5792@gmail.com>
2020-03-06 15:58:34 +0000

    setup of container files

--- jobswipe_front_end/src/App.js
+++ jobswipe_front_end/src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <h1>Welcome to 5 Guys Project</h1>
-  );
+  return <h1>Welcome to 5 Guys Project</h1>;
 }
 
 export default App;
