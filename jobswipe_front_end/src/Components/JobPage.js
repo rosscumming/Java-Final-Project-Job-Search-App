366fda69bb9ef6a99c53ff15f8cf3213ec9037a7
Gary Clark <gclark04432@live.co.uk>
2020-03-09 15:15:26 +0000

    can cycle through joblist one by one

--- jobswipe_front_end/src/Components/JobPage.js
+++ jobswipe_front_end/src/Components/JobPage.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 const JobPage = ({ jobs }) => {
-  const jobsList = jobs.map((job, id) => {
-    return <li key={id}>{job.jobTitle}</li>
-  })
-    return (
+
+  const [selectedJobIndex, setSelectedJobIndex] = useState(0)
+
+  if (!jobs.length) return null;
+  const job = jobs[selectedJobIndex]
+
+  return (
 
     <section>
       <h1>New Job Details</h1>
@@ -12,7 +17,7 @@ const JobPage = ({ jobs }) => {
       <h2>Job Title</h2>
         <button>LIKE</button><button>DISLIKE</button>
         <p>Description:</p><p>Salary: £</p>
-        <ul>{ jobsList }</ul>
+        <ul>{ job.jobTitle }<button onClick={() => setSelectedJobIndex(selectedJobIndex+1)}>Next Job</button></ul>
     </section>
   )
 
