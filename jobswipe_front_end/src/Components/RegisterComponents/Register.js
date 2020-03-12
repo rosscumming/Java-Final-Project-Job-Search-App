69b71072c9816c7dc900c7a053225156a33bc991
Ross <rosso5792@gmail.com>
2020-03-12 14:32:51 +0000

    fixed nav bar and added consistent logo across site

--- jobswipe_front_end/src/Components/RegisterComponents/Register.js
+++ jobswipe_front_end/src/Components/RegisterComponents/Register.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
-import './Register.css'
-import logo from '../../assets/JobSwipe_2.png'
+import { Link } from "react-router-dom";
+import "./Register.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Register extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -13,99 +12,101 @@ class Register extends Component {
       initial_salary: 0,
       salary_weight: 5,
       location: ""
-    }
-    this.handleNewUser = this.handleNewUser.bind(this)
-    this.handleNameChange = this.handleNameChange.bind(this)
-    this.handleSalaryChange = this.handleSalaryChange.bind(this)
-    this.handleLocationChange = this.handleLocationChange.bind(this)
-    this.isFormValid = this.isFormValid.bind(this)
+    };
+    this.handleNewUser = this.handleNewUser.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSalaryChange = this.handleSalaryChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.isFormValid = this.isFormValid.bind(this);
   }
 
-  handleNewUser(event){
-    if (!this.state.name || !this.state.salary || !this.state.location) return null;
+  handleNewUser(event) {
+    if (!this.state.name || !this.state.salary || !this.state.location)
+      return null;
     const newUser = {
       name: this.state.name,
       salary: this.state.salary,
       initial_salary: this.state.salary,
       salary_weight: this.state.salary_weight,
       location: this.state.location
-    }
-    this.props.onUserSubmit(newUser)
+    };
+    this.props.onUserSubmit(newUser);
   }
 
-  handleNameChange(event){
-    this.setState({ name: event.target.value})
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
   }
 
   handleSalaryChange(event) {
-    this.setState({ salary: event.target.value})
+    this.setState({ salary: event.target.value });
   }
 
   handleLocationChange(event) {
-    this.setState({ location: event.target.value})
+    this.setState({ location: event.target.value });
   }
 
   isFormValid() {
-    return this.state.name && this.state.salary && this.state.location ? './jobs' : './register'
+    return this.state.name && this.state.salary && this.state.location
+      ? "./jobs"
+      : "./register";
   }
 
-  render () {
-    return(
-    <body>
-      <header className="main-header"><a href='./'><img class="logo" src={logo}/></a></header>
+  render() {
+    return (
+      <body>
+        <header className="main-header">
+          <a href="/" className="logo">
+            {" "}
+            JOBSWIPE
+            <FontAwesomeIcon icon="hand-pointer" className="logo-fa" />
+          </a>
+        </header>
 
-      <article className="register-article">
-        <h1>Registration Page</h1>
-        <h3><span>Create a new </span>
-          <span className="jobswipe-logo"> &lt;JobSwipe&gt; </span>
-          <span> profile here!!</span>
-        </h3>
+        <article className="register-article">
+          <h1>Registration Page</h1>
+          <h3>
+            <span>Create a new </span>
+            <span className="jobswipe-logo"> &lt;JobSwipe&gt; </span>
+            <span> profile here!!</span>
+          </h3>
 
-        <form className="register-form">
-          <input type="text"
-            placeholder="Your Name"
-            value={this.state.name}
-            onChange={this.handleNameChange}
-            className="register-input"
-          />
+          <form className="register-form">
+            <input
+              type="text"
+              placeholder="Your Name"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              className="register-input"
+            />
 
-          <input type="number"
-            step='2000'
-            placeholder="Expected Salary"
-            value={this.state.salary}
-            onChange={this.handleSalaryChange}
-            className="register-input"
-          />
+            <input
+              type="number"
+              step="2000"
+              placeholder="Expected Salary"
+              value={this.state.salary}
+              onChange={this.handleSalaryChange}
+              className="register-input"
+            />
 
-          <input type="text"
-            placeholder="Location"
-            value={this.state.location}
-            onChange={this.handleLocationChange}
-            className="register-input"
-          />
-          <br/><br/>
-          <Link to={this.isFormValid} onClick={this.handleNewUser}>
-            <button type="submit" className="link-register">Register User</button>
-          </Link>
-      </form>
-
-    </article>
-  </body>
-    )
-
-
+            <input
+              type="text"
+              placeholder="Location"
+              value={this.state.location}
+              onChange={this.handleLocationChange}
+              className="register-input"
+            />
+            <br />
+            <br />
+            <Link to={this.isFormValid} onClick={this.handleNewUser}>
+              <button type="submit" className="link-register">
+                Register User
+              </button>
+            </Link>
+          </form>
+        </article>
+      </body>
+    );
   }
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export default Register;
--- jobswipe_front_end/src/index.js
+++ jobswipe_front_end/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCheckSquare,
   faCheckCircle,
